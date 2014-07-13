241a71f559618282f5e493c04b65c908583eb3eb
erkin <devnull@localhost>
2014-07-13 12:30:58 +0200

    stats crude

--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -3,6 +3,8 @@
  */
 public class PercolationStats {
 
+    private double[] percolationResults;
+
     public static void main(String[] args){}
 
     public PercolationStats(int N, int T){
@@ -13,6 +15,8 @@ public class PercolationStats {
         if ( T <= 0 ){
             throw new IllegalArgumentException("T must be bigger than 0");
         }
+
+        percolationResults = new double[T];
 
         //do the experiment for T times using NxN grids
         int experimentCount = 0;
@@ -32,20 +36,44 @@ public class PercolationStats {
             }
 
             double percolationThreshold = openSiteCount / (N*N);
-
-            //do further calculations
+            percolationResults[experimentCount] = percolationThreshold;
+            experimentCount++;
         }
+
+        StdOut.println("mean = " + mean());
+        StdOut.println("stddev = " + stddev());
+        StdOut.println("95% confidence interval = " + confidenceLo() + ", " + confidenceHi());
     }
     public double mean(){
-        throw new RuntimeException("Not Impl");
+        double sum = 0.0;
+        for ( int i = 0; i < percolationResults.length; i++ ){
+            sum += percolationResults[i];
+        }
+
+        return sum / percolationResults.length;
     }
     public double stddev(){
-        throw new RuntimeException("Not Impl");
+
+        double mean = mean();
+        double devSum = 0.0;
+        for ( int i = 0; i < percolationResults.length; i++ ){
+            double deviation = Math.pow(percolationResults[i] - mean,2);
+            devSum += deviation;
+        }
+
+        double devSq = devSum / percolationResults.length - 1;
+        return Math.sqrt(devSq);
     }
     public double confidenceLo(){
-        throw new RuntimeException("Not Impl");
+        double mean = mean();
+        double stdDev = stddev();
+
+        return mean - ((1.96*stdDev) / Math.sqrt(percolationResults.length) );
     }
     public double confidenceHi(){
-        throw new RuntimeException("Not Impl");
+        double mean = mean();
+        double stdDev = stddev();
+
+        return mean + ((1.96*stdDev) / Math.sqrt(percolationResults.length) );
     }
 }
